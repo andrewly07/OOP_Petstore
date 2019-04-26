72fd75650a91ae54fe4c46f733b0ad025fd8c9e1
katepark1009 <47448455+katepark1009@users.noreply.github.com>
2019-04-25 18:23:35 -0700

    Add files via upload

---
+++ KAP/store.js
@@ -0,0 +1,15 @@
+//holds human, pet
+class Store {
+    constructor() {
+        this.human = new Human() // parameter?
+        this.pet1 = new Panda()
+         //parameter?
+    }
+    livingroom() {
+        this.human.rest()
+    }
+
+    
+
+
+}
